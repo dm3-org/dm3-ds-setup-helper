7eaaa702fa83825e87e3cc11800c1e86ec98cdfe
malteish <malteish@corpus.io>
2024-05-16 11:35:26 +0200

    add basic ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useState } from "react";
 
 const App = () => {
+  const [ens, setEns] = useState("ens");
+  const [url, setUrl] = useState("url");
+  const [profile, setProfile] = useState("profile");
+
+  const handleEnsChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setEns(event.target.value);
+  };
+
+  const handleUrlChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setUrl(event.target.value);
+  };
+
+  function clickMe() {
+    // alert(`The config file creation is not implemented yet! Once it is, it will use these values:
+    // ${ens} and ${url}`);
+    setProfile(ens + url);
+  }
+
   return (
     <div>
       <div
@@ -12,7 +35,37 @@ const App = () => {
       >
         <ConnectButton />
       </div>
-      <div>Welcome</div>
+      <div>
+        <p>
+          <h1>Welcome</h1>
+        </p>
+        <p>
+          <h2>Step 1: config file and profile creation</h2>
+          To create the profile and config file, please connect the account the
+          delivery service will use. Also, we need this information:
+          <p>
+            ENS:
+            <input onChange={(event) => handleEnsChange(event)}></input>
+          </p>
+          <p>
+            URL:
+            <input onChange={(event) => handleUrlChange(event)}></input>
+          </p>
+          <div>
+            <button onClick={clickMe}>Create config file</button>
+          </div>
+        </p>
+        <p>
+          <h2>Step 2: profile publication</h2>
+          Now, please connect the account that controls the ENS domain so we can
+          publish the profile.
+          <p>Profile: {profile}</p>
+          <p>
+            URL:
+            <input type="file" />
+          </p>
+        </p>
+      </div>
     </div>
   );
 };
