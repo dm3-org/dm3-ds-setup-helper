0b7e965fd7f66709cbd11a4bd4db59c628b6568d
malteish <malteish@corpus.io>
2024-05-21 17:11:44 +0200

    add profile and config download

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
   DeliveryServiceProfile,
   DeliveryServiceProfileKeys,
 } from "@dm3-org/dm3-lib-profile";
+import * as YAML from "yaml";
 
 const App = () => {
   const [ens, setEns] = useState("ens");
@@ -31,9 +32,10 @@ const App = () => {
   useEffect(() => {
     (async () => {
       if (variables?.message && signMessageData) {
+        const signature = signMessageData;
         const recoveredAddress = await recoverMessageAddress({
           message: variables?.message,
-          signature: signMessageData,
+          signature: signature,
         });
         setRecoveredAddress(recoveredAddress);
 
@@ -45,6 +47,7 @@ const App = () => {
             await createStorageKey(signMessageData)
           ),
         };
+
         const profile: DeliveryServiceProfile = {
           publicEncryptionKey: keys.encryptionKeyPair.publicKey,
           publicSigningKey: keys.signingKeyPair.publicKey,
@@ -79,8 +82,6 @@ const App = () => {
   };
 
   function createConfigAndProfile() {
-    // alert(`The config file creation is not implemented yet! Once it is, it will use these values:
-    // ${ens} and ${url}`);
     const dsEnsAndUrl = JSON.stringify({
       ens: ens,
       url: url,
@@ -90,6 +91,27 @@ const App = () => {
         "I am creating the seed for a dm3 delivery service profile by signing this message." +
         dsEnsAndUrl,
     });
+  }
+
+  function storeProfile() {
+    const fileData = YAML.stringify(profile);
+    const blob = new Blob([fileData], { type: "text/plain" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = "dm3-delivery-service-profile_PUBLIC.yml";
+    link.href = url;
+    link.click();
+  }
+
+  function storeConfig() {
+    const config = { ...keys, ens: ens, rpc: url };
+    const fileData = YAML.stringify(config);
+    const blob = new Blob([fileData], { type: "text/plain" });
+    const buttonUrl = URL.createObjectURL(blob);
+    const buttonLink = document.createElement("a");
+    buttonLink.download = "dm3-delivery-service-config_PRIVATE.yml";
+    buttonLink.href = buttonUrl;
+    buttonLink.click();
   }
 
   function publishProfile() {
@@ -111,7 +133,7 @@ const App = () => {
         <div>
           <h1>Welcome {address}</h1>
           <div>
-            <h2>Step 1: config file and profile creation</h2>
+            <h2>Step 1: create config and profile</h2>
             To create the profile and config file, please connect the account
             the delivery service will use. Also, we need this information:
             <p>
@@ -124,31 +146,42 @@ const App = () => {
             </p>
             <div>
               <button onClick={createConfigAndProfile}>
-                Create config file
+                Create config and profile
               </button>
-              <p>{recoveredAddress.length > 0 ? recoveredAddress : ""}</p>
             </div>
           </div>
           <div>
-            <h2>Step 2: profile publication</h2>
-            Now, please connect the account that controls the ENS domain so we
-            can publish the profile.
-            <p>
-              Profile:{" "}
-              {profileAndKeysCreated
-                ? JSON.stringify(profile)
-                : "Finish step 1 first"}
-              Keys:{" "}
-              {profileAndKeysCreated
-                ? JSON.stringify(keys)
-                : "Finish step 1 first"}
-            </p>
-            <button onClick={publishProfile}>Publish profile</button>
-            {/* <p>
+            <h2>Step 2: store config and profile</h2>
+            <button disabled={!profileAndKeysCreated} onClick={storeProfile}>
+              Store profile file
+            </button>
+            {/* <p>{recoveredAddress.length > 0 ? recoveredAddress : ""}</p> */}
+            <button disabled={!profileAndKeysCreated} onClick={storeConfig}>
+              Store config
+            </button>
+          </div>
+        </div>
+        <div>
+          <h2>Step 3: publish profile</h2>
+          Now, please connect the account that controls the ENS domain so we can
+          publish the profile.
+          <p>
+            Profile:{" "}
+            {profileAndKeysCreated
+              ? JSON.stringify(profile)
+              : "Finish step 1 first"}
+            Keys:{" "}
+            {profileAndKeysCreated
+              ? JSON.stringify(keys)
+              : "Finish step 1 first"}
+          </p>
+          <button disabled={!profileAndKeysCreated} onClick={publishProfile}>
+            Publish profile
+          </button>
+          {/* <p>
             load profile from file (optional):
             <input type="file" />
           </p> */}
-          </div>
         </div>
       </div>
     </RainbowKitProvider>
